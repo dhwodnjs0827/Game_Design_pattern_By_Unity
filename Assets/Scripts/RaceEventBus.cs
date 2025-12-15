9be92c94306710f0c0c9b710ade652104a8cbb09
dhwodnjs <dhwodnjs0827@naver.com>
2025-12-15 16:18:51 +0900

    Initial commit

---
+++ Assets/Scripts/RaceEventBus.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+/// <summary>
+/// 전역에서 접근할 수 있는 이벤트 버스를 설정하고자 static 키워드를 사용하는 클래스 
+/// </summary>
+public class RaceEventBus : MonoBehaviour
+{
+    private static readonly IDictionary<RaceEventType, UnityEvent> Events = new Dictionary<RaceEventType, UnityEvent>();
+
+    public static void Subscribe(RaceEventType eventType, UnityAction listener)
+    {
+        if (Events.TryGetValue(eventType, out var thisEvent))
+        {
+            thisEvent.AddListener(listener);
+        }
+        else
+        {
+            thisEvent = new UnityEvent();
+            thisEvent.AddListener(listener);
+            Events.Add(eventType, thisEvent);
+        }
+    }
+
+    public static void Unsubscribe(RaceEventType eventType, UnityAction listener)
+    {
+        if (Events.TryGetValue(eventType, out var thisEvent))
+        {
+            thisEvent.RemoveListener(listener);
+        }
+    }
+
+    public static void Publish(RaceEventType eventType, UnityEvent unityEvent)
+    {
+        if (Events.TryGetValue(eventType, out var thisEvent))
+        {
+            thisEvent.Invoke();
+        }
+    }
+}
+
+public enum RaceEventType
+{
+    
+}
